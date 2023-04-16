9dc426b978172b4a8fdf14c8692af1fe23820725
Artyom Pizin <artempetr2016@gmail.com>
2023-04-16 20:42:36 +0300

    feat: implement Stack class

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push( element ) {
+
+    if ( !this._stack ) {
+
+      this._stack = [];
+
+    }
+
+    this._stack.push( element );
+
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if ( !this._stack ) {
+
+      this._stack = [];
+
+    }
+
+    return this._stack.pop();
+
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    if ( !this._stack ) {
+
+      this._stack = [];
+
+    }
+
+    return this._stack[ this._stack.length - 1 ];
+
   }
 }
 
